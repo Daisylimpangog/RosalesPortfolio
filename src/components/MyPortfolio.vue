10e04639582fb54c3b1321baa8781810a86c2634
Daisylimpangog <119760453+Daisylimpangog@users.noreply.github.com>
2023-12-14 12:29:56 +0800

    new1

--- src/components/MyPortfolio.vue
+++ src/components/MyPortfolio.vue
@@ -26,76 +26,69 @@
     </header>
 
     <section class="home">
-      <div class="home-content">
+      <div class="home-content2">
         <h3>Hello, It's Me</h3>
-        <h1>User Name</h1>
-        <!-- <h1>Daisy A. Limpangog</h1> -->
+        <h1>John Lloyd S. Rosales</h1>
         <p>Web Designer</p>
 
         <div class="home-sci">
           <a
-            href="https://www.facebook.com/daisy.jroa"
+            href="https://www.facebook.com/john.clair.28"
             target="_blank"
-            :style="{ '--i': 7 }"
+            :style="{ '--i': 2 }"
           >
             <i class="bx bxl-facebook"></i>
           </a>
           <a
-            href="https://www.instagram.com/Followed%20by%20189%2C271%20people/?fbclid=IwAR1rIl4vpvTIqjXl609QXyz1_wab6kps5eDvR9mgJ7zxjTm8oEiiumflshs"
+            href="https://www.instagram.com/its_johnx/"
             :style="{ '--i': 8 }"
             target="_blank"
           >
             <i class="bx bxl-instagram"></i>
           </a>
           <a
-            href="https://github.com/Daisylimpangog"
+            href="https://github.com/Rosales1124"
             target="_blank"
             :style="{ '--i': 9 }"
           >
             <i class="bx bxl-github"></i>
           </a>
           <a
-            href="https://l.facebook.com/l.php?u=https%3A%2F%2Ftiktok.com%2F%40Followed%2520by%25201%252C723%252C927%2520people%3Ffbclid%3DIwAR0abiLyFwMObJ4R2EOOgY5ljn-tAzCMBOInWgFeZ0Q4uPnbxQxZUgu3iPU&h=AT1XEELZ7laqgf5RJFJJ1kij4qb_gxl3uFlvQtyd3421AQLALrLqZ-rsP7EMSO2kOotU_jUi_hVj43QRLkbPGsmrXunid0XYuzInqe2k8B9eWFaXAFp-Kvnh6U-TvZhSHrAC2OjtFPpByFiK3KGM"
+            href="https://www.tiktok.com/@user492397399?lang=en"
             :style="{ '--i': 10 }"
             target="_blank"
           >
             <i class="bx bxl-tiktok"></i>
           </a>
         </div>
-        <a href="#" class="btn-box">More About Me</a>
+        <a href="about-text" class="btn-box">About Me</a>
       </div>
       <div class="images">
-        <img :src="daisy" />
+        <img :src="rosales" />
       </div>
       <span class="home-imgHover"></span>
     </section>
 
     <section class="about" id="about">
       <div class="about-img">
-        <img :src="daisy" />
+        <img :src="rosales" />
       </div>
       <div class="about-text">
         <h2>About <span>Me</span></h2>
         <h4>Who I Am</h4>
         <p>
-          <!-- I am a 25-year-old male who consistently tells my teacher that I am a
-          girl because my name is feminine, despite my actual gender being male.
-          I have a strong inclination for social interaction, enjoying meeting
-          new people and forming friendships. I am sociable, frequently going
-          out with friends and family. Additionally, I have a passion for
-          exploring new places, with a particular affinity for nature. I also
-          have a keen interest in creative activities such as video and photo
-          editing. -->
-          his info
+          I am John Lloyd Rosales, 24 years old. I have four members in the family including myself, my mom and dad is a street vendor
+          they give me a reason to study hard so that i can give back to them and give the life that they deserve. I am passionate and hardworking
+          person, also I am friendly and helpful. In addition, I am an aspirant programmer, and hopefully i can claim myself as a programmer in the near future
         </p>
         <a href="#" class="btn-box">More About Me</a>
       </div>
     </section>
 
     <section style="display: flex; height: 100vh">
-      <!-- <h1 className="sub-title" style="font-size: 50px; margin-bottom: -20px;">My <span>Skills</span></h1> -->
+      
       <div class="container1" id="skills">
-        <h1 class="heading1">My <span style="color: #0ef">Skills</span></h1>
+        <h1 class="heading1">My <span style="color: rgb(255, 0, 34)">Skills</span></h1>
         <div class="technical-bars">
           <div v-for="(skill, index) in skills" :key="index" class="bar">
             <i :class="skill.iconClass" :style="{ color: skill.color }"></i>
@@ -111,54 +104,69 @@
     </section>
 
     <section id="todo-list">
-      <div class="todo-container">
-        <div class="todo-body">
-          <h1>Todo List App</h1>
-          <div class="input-list">
-            <div class="search-list">
-              <input
-                type="text"
-                v-model="searchText"
-                placeholder="Search Todo..."
-              />
-              <i class="bx bx-search-alt-2"></i>
-            </div>
+      <div class="container2">
+      <div class="title2"><h1>Todo App</h1></div>
+      <div class="actions2">
+        <form @submit="addTask">
+          <input type="text" placeholder="Add Task" v-model="task" required />
+          <button type="submit" class="add">Add</button>
+          <div class="search">
+            <input
+              type="text"
+              placeholder="Search task"
+              v-model="searchTerm"
+              @input="searchTask"
+            />
           </div>
-          <div v-for="(todo, index) in todos" :key="index" class="todo-list">
-            <p :class="{ completed: todo.completed }">{{ todo.text }}</p>
-            <!-- <div> -->
-            <i @click="toggleCompletionStatus(index)" class="bx bx-check"></i>
-            <i
-              @click="removeTodo(index)"
-              class="bx bx-x"
-              v-if="!todo.completed"
-            ></i>
-            <!-- </div> -->
-          </div>
-          <div class="clear-all"><button @click="clearAll" v-if="incompleteTodosExist">Clear All</button></div>
-
-          <div class="add-todo">
-            <h1>Add New List</h1>
-            <div class="new-input">
-              <input
-                type="text"
-                v-model="newTodo"
-                placeholder="Write Here..."
-              />
-              <i @click="addTodo" class="bx bx-plus"></i>
-            </div>
-          </div>
+        </form>
+      </div>
+      <div class="tasks">
+        <div class="task-items" v-for="todo in filteredTodos" :key="todo.id">
+          <p :class="{ 'done-task': todo.status }">{{ todo.details }}</p>
+          <button class="done-btn" @click="toggleTaskStatus(todo.id)">
+            Done
+          </button>
+          <button class="remove-btn" @click="removeTask(todo.id)">Remove</button>
+        </div>
+        <button
+          class="clear-btn"
+          @click="clearAllTasks"
+          v-show="todos.length > 0"
+        >
+          Clear Task
+        </button>
+      </div>
+    </div>
+    </section>
+    <br>
+    <br>
+    <br>
+      <section id="alarmclock">
+        <h1 style="text-align: center; font-size: 30px;">Alarm Clock Application</h1>
+        <br>
+    <div class="container3">
+      <h1>Alarm Clock</h1>
+      <div class="clock">
+        <div id="time"></div>
+        <div id="date"></div>
+      </div>
+      <div class="controls">
+        <label for="alarmTime">Set Alarm:</label>
+        <input type="time" id="alarmTime" />
+        <div class="btn2">
+          <button @click="setAlarm" id="setAlarm">Set</button>
+<button @click="clearAlarm">Clear</button>
         </div>
       </div>
-    </section>
-
+    </div>
+  </section>
     <section>
       <div id="contact">
         <div class="container">
           <div class="rowd">
             <div class="contact-left">
               <h1 class="sub-title">
-                Contact <span style="color: #0ef">Me</span>
+                Contact <span style="color: rgb(123, 0, 255)">Me</span>
               </h1>
               <p class="emails">
                 <i class="bx bx-send"></i> {{ contactEmail }}
@@ -166,28 +174,28 @@
               <p><i class="bx bx-phone"></i> {{ contactPhoneNumber }}</p>
               <div class="home-sci">
                 <a
-                  href="https://www.facebook.com/daisy.jroa"
+                  href="https://www.facebook.com/john.clair.28"
                   target="_blank"
                   :style="{ '--i': 7 }"
                 >
                   <i class="bx bxl-facebook"></i>
                 </a>
                 <a
-                  href="https://www.instagram.com/Followed%20by%20189%2C271%20people/?fbclid=IwAR1rIl4vpvTIqjXl609QXyz1_wab6kps5eDvR9mgJ7zxjTm8oEiiumflshs"
+                  href="https://www.instagram.com/its_johnx/"
                   :style="{ '--i': 8 }"
                   target="_blank"
                 >
                   <i class="bx bxl-instagram"></i>
                 </a>
                 <a
-                  href="https://github.com/Daisylimpangog"
+                  href="https://github.com/Rosales1124"
                   target="_blank"
                   :style="{ '--i': 9 }"
                 >
                   <i class="bx bxl-github"></i>
                 </a>
                 <a
-                  href="https://l.facebook.com/l.php?u=https%3A%2F%2Ftiktok.com%2F%40Followed%2520by%25201%252C723%252C927%2520people%3Ffbclid%3DIwAR0abiLyFwMObJ4R2EOOgY5ljn-tAzCMBOInWgFeZ0Q4uPnbxQxZUgu3iPU&h=AT1XEELZ7laqgf5RJFJJ1kij4qb_gxl3uFlvQtyd3421AQLALrLqZ-rsP7EMSO2kOotU_jUi_hVj43QRLkbPGsmrXunid0XYuzInqe2k8B9eWFaXAFp-Kvnh6U-TvZhSHrAC2OjtFPpByFiK3KGM"
+                  href="https://www.tiktok.com/@user492397399?lang=en"
                   :style="{ '--i': 10 }"
                   target="_blank"
                 >
@@ -196,29 +204,29 @@
               </div>
               <a href="#" class="btn btn2">Download CV</a>
             </div>
-            <div class="contact-right">
-              <form @submit.prevent="submitForm">
-                <input
-                  type="text"
-                  v-model="name"
-                  placeholder="Your Name"
-                  required
-                />
-                <input
-                  type="email"
-                  v-model="email"
-                  placeholder="Your@gmail.com"
-                  required
-                />
-                <textarea
-                  v-model="message"
-                  placeholder="Your Message"
-                  cols="30"
-                  rows="10"
-                ></textarea>
-                <button type="submit" class="btn btn2">Submit</button>
-              </form>
-            </div>
+            <div class="container4">
+  <form action="tambokpinoy22@gmail.com">
+
+    <label for="fname">First Name</label>
+    <input type="text" id="fname" name="firstname" placeholder="Your name..">
+
+    <label for="lname">Last Name</label>
+    <input type="text" id="lname" name="lastname" placeholder="Your last name..">
+
+    <label for="country">Country</label>
+    <select id="country" name="country">
+      <option value="australia">Australia</option>
+      <option value="canada">Canada</option>
+      <option value="usa">USA</option>
+    </select>
+
+    <label for="subject">Subject</label>
+    <textarea id="subject" name="subject" placeholder="Write something.." style="height:200px"></textarea>
+
+    <input type="submit" value="Submit">
+
+  </form>
+</div>
           </div>
         </div>
       </div>
@@ -238,21 +246,19 @@
   text-align: center; 
 }
 </style>
-
 <script>
 import "../assets/css/Style.css";
-
-// boxicons
 import "boxicons/css/boxicons.min.css";
+import { v4 as uuidv4 } from "uuid";
 
 export default {
   data() {
     return {
       navBar: false,
       logo: require("../assets/images/logo.jpg"),
-      daisy: require("../assets/images/daisy.jpg"),
-      contactEmail: "Daisylimpangog@gmail.com",
-      contactPhoneNumber: "09204890541",
+      rosales: require("../assets/images/rosales.jpg"),
+      contactEmail: "tambokpinoy22@gmail.com",
+      contactPhoneNumber: "09455898071",
       socialLinks: {
         facebook: "#",
         twitter: "#",
@@ -263,16 +269,7 @@ export default {
       email: "",
       message: "",
       searchText: "",
-      newTodo: "",
-      todos: [
-        {
-          text: "Bytewebster is a website where we create amazing",
-        },
-        {
-          text: "Digital Clok in Java Script or CSS - ByteWebster",
-        },
-        { text: "JavaScript Quiz web App using Glassmorphism - ByteWebster" },
-      ],
+
       skills: [
         {
           name: "HTML",
@@ -293,11 +290,19 @@ export default {
           class: "javascript",
         },
       ],
+
+      task: "",
+      todos: [],
+      searchTerm: "",
+      foundTasks: [], // New property to store found tasks
     };
   },
   computed: {
     incompleteTodosExist() {
       return this.todos.some((todo) => !todo.completed || todo.completed);
+    },
+    filteredTodos() {
+      return this.foundTasks.length > 0 ? this.foundTasks : this.todos;
     },
   },
   methods: {
@@ -310,25 +315,91 @@ export default {
     submitForm() {
       // Handle form submission here
     },
-    addTodo() {
-      if (this.newTodo.trim() !== "") {
-        this.todos.push({ text: this.newTodo });
-        this.newTodo = "";
+    toggleTaskStatus(id) {
+      const task = this.todos.find((todo) => todo.id === id);
+      if (task) {
+        task.status = !task.status;
       }
     },
-    removeTodo(index) {
-      this.todos.splice(index, 1);
+    addTask(e) {
+      e.preventDefault();
+      const newTask = {
+        id: uuidv4(),
+        details: this.task,
+        status: false,
+      };
+      this.todos.unshift(newTask);
+      this.task = "";
+    },
+    removeTask(id) {
+      const index = this.todos.findIndex((todo) => todo.id === id);
+      if (index !== -1) {
+        this.todos.splice(index, 1);
+      }
+    },
+    clearAllTasks() {
+      this.todos = [];
+    },
+    searchTask() {
+      this.foundTasks = this.todos.filter((todo) => {
+        return todo.details
+          .toLowerCase()
+          .includes(this.searchTerm.toLowerCase());
+      });
     },
 
-    toggleCompletionStatus(index) {
-      this.todos[index].completed = !this.todos[index].completed;
+    updateClock() {
+      const now = new Date();
+      const timeElement = document.getElementById("time");
+      const dateElement = document.getElementById("date");
+
+      const hours = now.getHours().toString().padStart(2, "0");
+      const minutes = now.getMinutes().toString().padStart(2, "0");
+      const seconds = now.getSeconds().toString().padStart(2, "0");
+      const timeString = `${hours}:${minutes}:${seconds}`;
+
+      const options = {
+        weekday: "long",
+        year: "numeric",
+        month: "long",
+        day: "numeric",
+      };
+      const dateString = now.toLocaleDateString("en-US", options);
+
+      timeElement.textContent = timeString;
+      dateElement.textContent = dateString;
     },
 
-    clearAll() {
-      this.todos = this.todos.filter(
-        (todo) => !todo.completed && todo.completed
-      );
+    setAlarm() {
+      const alarmTimeInput = document.getElementById("alarmTime");
+      const alarmTime = alarmTimeInput.value;
+
+      if (alarmTime) {
+        const now = new Date();
+        const alarmDateTime = new Date(now.toDateString() + " " + alarmTime);
+
+        const timeDifference = alarmDateTime - now;
+
+        if (timeDifference > 0) {
+          setTimeout(() => {
+            alert("Alarm!");
+          }, timeDifference);
+        } else {
+          alert("Please set a future time for the alarm.");
+        }
+      } else {
+        alert("Please set a time for the alarm.");
+      }
     },
+
+    clearAlarm() {
+      const alarmTimeInput = document.getElementById("alarmTime");
+      alarmTimeInput.value = "";
+    },
+  },
+  mounted() {
+    setInterval(this.updateClock, 1000);
+    this.updateClock();
   },
 };
 </script>
